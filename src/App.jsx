3c2a14172905a6c997077d8f4ca379a64b2d148d
Vinícius <vinicius.m.sousa3009@gmail.com>
2024-09-04 22:08:00 -0300

    Adicionado a aula  de api

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Header />
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<ApiRickAndMorty />} />
         <Route path="/about" element={<About />} />
       </Routes>
 
---
+++ src/pages/ApiRickAndMorty/index.jsx
@@ -0,0 +1,21 @@
+import './styles.css'
+
+const MockCharacter = [
+
+]
+
+export default function ApiRickAndMorty() {
+
+    function CarregarTodosOsPersonagens() {
+
+    }
+    return {
+        info: {},
+            result: MockCharacter
+        
+    }
+
+    function listaPersonagens(){
+        const { result }
+    }
+}
